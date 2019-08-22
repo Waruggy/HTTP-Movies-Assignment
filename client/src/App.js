79e3e5404f8226eeb52299afb575136a8ee29df9
Charles <cwang8413@yahoo.com>
2019-08-22 16:01:29 -0700

    Looks good.

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import MovieForm from "./Movies/MovieForm";
+import AddMovie from "./Movies/AddMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+ const [movieToUpdate, setMovieToUpdate] = useState();
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
@@ -15,12 +18,14 @@ const App = () => {
     <>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
+      <Route exact path="/add-movie" component={AddMovie} />
       <Route
         path="/movies/:id"
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
       />
+       <Route path="/edit-movie/:id" component={MovieForm} />
     </>
   );
 };
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import axios from 'axios'
+import { Link } from "react-router-dom";
 
 const MovieCard = props => {
-  const { title, director, metascore, stars } = props.movie;
+  const { title, director, metascore, stars, id } = props.movie;
+    
+    const deleteMovie = (e) => {
+    e.preventDefault();
+    console.log('Delete Movie');
+    axios
+      .delete(`http://localhost:5000/api/movies/${id}`)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err.response))
+  }
+
   return (
     <div className="movie-card">
       <h2>{title}</h2>
@@ -18,6 +32,14 @@ const MovieCard = props => {
           {star}
         </div>
       ))}
+     <div className="buttons">
+      <div>
+        <Link to={`/edit-movie/${id}`}>
+        <button type="button">Edit</button>
+        </Link>
+      </div>
+      <button type="button" onClick={deleteMovie}>Delete</button>
+      </div>
     </div>
   );
 };
